d42927810d74f95b3d544a511c4428ba13efe20f
Milzam Radifan <milzam_radifan@student.ub.ac.id>
2024-10-07 13:03:31 +0700

    Initial commit

---
+++ app/src/main/java/com/example/dailydo/Home/HomeActivity.java
@@ -0,0 +1,22 @@
+package com.example.dailydo.Home;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.dailydo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    EdgeToEdge.enable(this);
+    setContentView(R.layout.activity_main);
+
+
+
+
+  }
+}
